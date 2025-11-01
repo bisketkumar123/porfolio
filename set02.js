76e88963b602a8e5eb916cbdd3626daccecbb3cf
bisketkumar123 <itsah2099@gmail.com>
2025-11-01 16:13:34 +0530

    Add files via upload

---
+++ set02.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".bk", {
+    strings: ["Bisket kumar", "Frontend Developer", "Youtuber", "Web Developer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:500,
+    loop: true
+});
